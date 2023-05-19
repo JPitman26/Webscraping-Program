107531759d8711f4586a6a89ed709448bb01235a
JackPitman <80924402+JPitman26@users.noreply.github.com>
2023-05-19 16:19:26 +0100

    Add HTTP request and HTML parsing functionality
    Implement the code to send an HTTP request to the target website and parse the HTML response using BeautifulSoup

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+def scrape_product_price(url):
+    # Make a request to the product page
+    response = requests.get(url)
+    
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(response.text, "html.parser")
+    
