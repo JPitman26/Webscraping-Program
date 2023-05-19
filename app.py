bd08fcca51122a4f9c3d33ecb078c81ce51b70ec
JackPitman <80924402+JPitman26@users.noreply.github.com>
2023-05-19 16:22:25 +0100

    Add error handling and edge case scenarios
    Improve the program by implementing error handling for cases where the price element cannot be found or when there are issues with the website or URL

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ def scrape_product_price(url):
     # Create a BeautifulSoup object
     soup = BeautifulSoup(response.text, "html.parser")
     
+    # Find the element containing the price
+    price_element = soup.find("div", class_="product-price")
+    if price_element:
+        price = price_element.text.strip()
+        return price
+    else:
+        return "Price not found."
+
+# Example usage
+product_url = "example-product.com"
+price = scrape_product_price(product_url)
+print("Price:", price)
